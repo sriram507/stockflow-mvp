2b2ac949d99a0481ac1dab2ea49a4c9cbbf75e8e
sriram507 <isreeram2014@gmail.com>
2025-12-19 14:05:46 +0530

    Fix import casing for Vercel deployment

--- frontend/vite-project/src/api.js
+++ frontend/vite-project/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:4000" });
+const api = axios.create({ baseURL: "https://stockflow-mvp.onrender.com" });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
